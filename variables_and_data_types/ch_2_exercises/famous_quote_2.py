085d2ba119dc1699fa876cc22e9a29a6753c4f9b
natalie <natgiselle29@gmail.com>
2025-09-01 18:49:59 -0400

    modifies format on exercise 2.6

--- variables_and_data_types/ch_2_exercises/famous_quote_2.py
+++ variables_and_data_types/ch_2_exercises/famous_quote_2.py
@@ -1,4 +1,5 @@
-# Exercise 2-6: Famous Quote 2: Repeat Exercise 2-5, but this time 
+# Exercise 2-6: Famous Quote 2
+# Repeat Exercise 2-5, but this time 
 # store the famous person’s name in a variable called famous_person. 
 # Then compose your message and store it in a new variable called message. 
 # Print your message.
