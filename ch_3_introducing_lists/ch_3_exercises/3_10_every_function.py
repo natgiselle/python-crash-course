952304b1660cd5dec049b4d089b8d253c0d89f01
natalie <natgiselle29@gmail.com>
2026-03-21 15:01:19 -0400

    adds ch 3

---
+++ ch_3_introducing_lists/ch_3_exercises/3_10_every_function.py
@@ -0,0 +1,20 @@
+# Exercise 3.10: Every Function
+
+"""
+Think of something you could store in a list. 
+For example, you could make a list of:
+mountains, rivers, countries, cities, languages, or anything else you'd like. 
+
+Write a program that creates a list containing these items and then 
+uses each function introduced in this chapter at least once.
+"""
+
+# Each function used:
+# .append(object) method
+# .insert(object) -
+# .remove(object) -only deletes first occurence of the specified value
+# .pop()
+# sorted()
+# sort()
+# len()
+# reverse()
