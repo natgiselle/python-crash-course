6a92b9a472112815fee586f447c1748ffbad4f17
natalie <natgiselle29@gmail.com>
2026-03-21 19:30:48 -0400

    creates exercise 4.10 file

---
+++ ch_4_working_with_lists/4_10_slices.py
@@ -0,0 +1,17 @@
+# Exercise 4.10: Slices
+'''
+Using one of the programs you wrote in this chapter, add several
+lines to the end of the program that do the following:
+
+Print the message, The first three items in the list are:
+Then use a slice to print the first three items from that program's list.
+
+Print the message, Three items from the middle of the list are:
+Use a slice to print three items from the middle of the list.
+
+Print the message, The last three items in the list are:
+Use a slice to print the last three items in the list 
+'''
+
+# modifying list from 4_2_animals.py
+animals = ['cat', 'dog', 'bird', 'hamster', 'guinea pig', 'turtle', 'goldfish', 'crab', 'snail', 'lizard']
