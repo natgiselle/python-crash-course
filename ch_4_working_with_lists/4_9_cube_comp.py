5d5ffeb340163ef9130e64d2b5cbe4d9fbb82004
natalie <natgiselle29@gmail.com>
2026-03-21 16:41:38 -0400

    adds exercise 4.9

--- ch_4_working_with_lists/4_9_cube_comp.py
+++ ch_4_working_with_lists/4_9_cube_comp.py
@@ -2,6 +2,7 @@
 '''
 Use a list comprehension to generate a list of the first 10 cubes.
 '''
-
-# is what youve been doing
-
+[print(cube ** 3) for cube in range(1, 11)] # if it wasnt asking to store values in a list you could do print
+cubes = [cube ** 3 for cube in range(1, 11)] # are also allowed to use print function when doing list comprehension but WONT save the values in the list!!
+for num in cubes:
+    print(num)
